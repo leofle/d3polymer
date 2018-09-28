231e62cb2fe90a6f787acc37f947317f3ae9d4a8
leofle <leofle@gmail.com>
2018-09-28 09:02:13 +0300

    fix style to match rest

--- src/my-view1.js
+++ src/my-view1.js
@@ -17,8 +17,6 @@ class MyView1 extends PolymerElement {
       <style include="shared-styles">
         :host {
           display: block;
-
-          padding: 10px;
         }
         h1 {
           font-size: 1.5em;
